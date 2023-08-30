a79549559f7c450106e3d8420ca1307bdc045fef
Veronica Rojas <ar_veronica@yahoo.com>
2023-08-30 17:01:47 +0100

    Organize component folders and add CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   
 
   return (
     <>
-    <img
-    src="src/assets/react.svg"
-    alt="react logo"/>
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-      
+    <Header />
+    <MainContent />
+    <Footer /> 
+    
+
+   
+    
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+    <nav className='nav'>
+      <img
+      src="src/assets/react.svg"
+      alt="react logo"
+      className='react-logo'/>
+      <ul className='nav-items'>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+    </nav>
+    </header>
+  )
+}
