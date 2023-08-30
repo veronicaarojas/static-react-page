6d10443f24ba5ca9b349d441e52b9e6db5b75179
Veronica Rojas <ar_veronica@yahoo.com>
2023-08-30 19:31:32 +0100

    edits to navbar styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,14 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <header className='header'>
+    <header>
     <nav className='nav'>
       <img
       src="src/assets/react.svg"
       alt="react logo"
       className='react-logo'/>
-      <ul className='nav-items'>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact</li>
-    </ul>
+      <h3 className='navbar-title'>ReactFacts</h3>
+      <h4>React Course - Project 1</h4>
     </nav>
     </header>
   )
