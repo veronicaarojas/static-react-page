ca054b3c97e50b85060dcd78cf97059fe9d163f3
Veronica Rojas <ar_veronica@yahoo.com>
2023-08-30 19:08:21 +0100

    initial styles

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function MainContent() {
   return (
   <div className='main-content'>
-      <h1>Fun facts about React</h1>
-    <ul>
+      <h1 className='content-title'>Fun facts about React</h1>
+    <ul className='main-content-items'>
       <li>Was first released in 2013</li>
       <li>Was originally created by Jordan Walke</li>
       <li>Has well over 100k stars on Github</li>
