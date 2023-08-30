4cab2b4ca1a80b8dd44e672c46280b2b590ecd1b
Veronica Rojas <ar_veronica@yahoo.com>
2023-08-30 15:42:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <img
+    src="src/assets/react.svg"
+    alt="react logo"/>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+      
+    </>
+  )
+}
+
+export default App
